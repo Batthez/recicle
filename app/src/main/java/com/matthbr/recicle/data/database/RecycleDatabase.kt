fc4d77581f96f95a33a795a8ceee33fb783cf62b
matheus.geiser <brhbs0948@ambev.com.br>
2020-11-17 13:12:36 -0300

    refactoring StoreFactory and RecycleDatabase

--- app/src/main/java/com/matthbr/recicle/data/database/RecycleDatabase.kt
+++ app/src/main/java/com/matthbr/recicle/data/database/RecycleDatabase.kt
@@ -1,6 +1,8 @@
 package com.matthbr.recicle.data.database
 
+import android.content.Context
 import androidx.room.Database
+import androidx.room.Room
 import androidx.room.RoomDatabase
 import com.matthbr.recicle.domain.model.Item
 
@@ -15,4 +17,21 @@ import com.matthbr.recicle.domain.model.Item
 
 abstract class RecycleDatabase : RoomDatabase(){
     abstract fun itemDao(): ItemDAO
+
+    companion object {
+        @Volatile
+        private var INSTANCE : RecycleDatabase? = null
+
+        fun getDatabase(context : Context) : RecycleDatabase{
+
+            return INSTANCE ?: synchronized(this){
+                val instance = Room.databaseBuilder(
+                        context.applicationContext, RecycleDatabase::class.java,
+                        "recycle_database"
+                ).build()
+                INSTANCE = instance
+                instance
+            }
+        }
+    }
 }
--- app/src/main/java/com/matthbr/recicle/view/store/StoreFactory.kt
+++ app/src/main/java/com/matthbr/recicle/view/store/StoreFactory.kt
@@ -1,11 +1,18 @@
 package com.matthbr.recicle.view.store
 
+import android.content.Context
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import com.matthbr.recicle.data.database.RecycleDatabase
+import com.matthbr.recicle.data.repository.ItemListRepositoryImpl
 
-class StoreFactory : ViewModelProvider.Factory{
+class StoreFactory(private val context: Context) : ViewModelProvider.Factory{
     override fun <T : ViewModel?> create(modelClass: Class<T>): T {
-        return
+        return ItemDetailsStore(
+                ItemListRepositoryImpl(
+                        RecycleDatabase.getDatabase(context).itemDao()
+                )
+        ) as T
     }
 
 }
