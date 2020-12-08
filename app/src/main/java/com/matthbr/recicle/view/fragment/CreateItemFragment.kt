76e1d2ca92a6af60f7943565783d3009603f8029
Matheus Geiser <matheus.lgeiser@ambevtech.com.br>
2020-12-08 19:48:08 -0300

    alteração no layout do item e adicionando algumas verificações na hora de cadastrar

--- app/src/main/java/com/matthbr/recicle/view/fragment/CreateItemFragment.kt
+++ app/src/main/java/com/matthbr/recicle/view/fragment/CreateItemFragment.kt
@@ -1,93 +1,99 @@
 package com.matthbr.recicle.view.fragment
 
-import android.app.AlertDialog
-import android.content.DialogInterface
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
-import androidx.navigation.fragment.findNavController
 import com.matthbr.recicle.R
 import com.matthbr.recicle.mvi.MVI
 import com.matthbr.recicle.view.store.ItemStore
 import com.matthbr.recicle.view.store.StoreFactory
 import kotlinx.android.synthetic.main.fragment_second.*
-import kotlinx.android.synthetic.main.item_list.*
-import kotlinx.coroutines.InternalCoroutinesApi
 
 class CreateItemFragment : Fragment() {
 
-    private lateinit var store : ItemStore
+    private lateinit var store: ItemStore
     private lateinit var storeFactory: StoreFactory
+    private lateinit var informationDialog: AlertDialog
 
     override fun onCreateView(
-            inflater: LayoutInflater, container: ViewGroup?,
-            savedInstanceState: Bundle?
-    ): View?
-        = inflater.inflate(R.layout.fragment_second, container, false)
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? = inflater.inflate(R.layout.fragment_second, container, false)
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         initStore()
-        button_second.setOnClickListener{
+        button_second.setOnClickListener {
             insertNewItem()
         }
     }
 
-    private fun fieldsOk() : Boolean{
+    private fun fieldsOk(): Boolean {
         val description = text_descricao.text.toString()
         val quantity = text_quantidade.text.toString()
         return !description.trim().isEmpty() && !quantity.trim().isEmpty()
     }
 
-    private fun initStore(){
+    private fun initStore() {
         storeFactory = StoreFactory(requireContext())
-        store = ViewModelProvider(viewModelStore,storeFactory).get(ItemStore::class.java)
+        store = ViewModelProvider(viewModelStore, storeFactory).get(ItemStore::class.java)
         store.state.observe(viewLifecycleOwner, stateObserver())
     }
 
-    private fun handleLoadingIndicator(loading : Boolean){
-        button_second.isClickable = !loading
+    private fun disableButtonClickability() {
+        button_second.isClickable = false
     }
 
-    private fun insertNewItem(){
-        if(fieldsOk()){
+    private fun enableButtonClickability() {
+        button_second.isClickable = true
+    }
+
+    private fun insertNewItem() {
+        if (fieldsOk()) {
             val description = text_descricao.text.toString().trim()
             val quantity = text_quantidade.text.toString().toInt()
-            store.actionInsertItem(description,quantity)
-        }else{
-            Toast.makeText(requireContext(),getString(R.string.preencha_os_campos), Toast.LENGTH_LONG).show()
+            store.actionInsertItem(description, quantity)
+            disableButtonClickability()
+        } else {
+            Toast.makeText(
+                requireContext(),
+                getString(R.string.preencha_os_campos),
+                Toast.LENGTH_LONG
+            ).show()
         }
     }
 
     private fun stateObserver() = Observer<MVI.Store.State<ItemStore.Data>> { state ->
-        with(state.data){
-            handleLoadingIndicator(state.loading)
-
-            state.message?.let {
-                if(it.type == MVI.Store.MessageType.SUCCESS){
-                    dialog(it.message!!)
-                }
-
+        with(state.data) {
+            if (state.message?.type == MVI.Store.MessageType.SUCCESS) {
+                dialog(state.message.message!!)
+                enableButtonClickability()
             }
         }
     }
 
-    private fun dialog(text : String){
-        val dialogBuilder = AlertDialog.Builder(requireContext())
-        dialogBuilder.setTitle(text)
-        dialogBuilder.setCancelable(false)
-        dialogBuilder.setPositiveButton(getString(R.string.ok)) { _, _ ->
-            findNavController().navigateUp()
+    private fun dialog(text: String) {
+        val builder = AlertDialog.Builder(requireContext())
+        with(builder) {
+            setTitle(text)
+            setCancelable(false)
+            setPositiveButton(getString(R.string.ok)) { _, _ -> clearFields() }
         }
-        dialogBuilder.show()
+
+        informationDialog = builder.create()
+        informationDialog.show()
+
+        store.actionClearMessage()
     }
 
-    private fun clearFields(){
-
+    private fun clearFields() {
+        text_quantidade.setText("")
+        text_descricao.setText("")
     }
 }
--- app/src/main/java/com/matthbr/recicle/view/store/ItemStore.kt
+++ app/src/main/java/com/matthbr/recicle/view/store/ItemStore.kt
@@ -3,36 +3,34 @@ package com.matthbr.recicle.view.store
 import com.matthbr.recicle.data.repository.ItemListRepositoryImpl
 import com.matthbr.recicle.domain.model.Item
 import com.matthbr.recicle.mvi.MVI
-import kotlinx.coroutines.ExperimentalCoroutinesApi
-import kotlinx.coroutines.InternalCoroutinesApi
 
 class ItemStore(
-    private val itemRepository  : ItemListRepositoryImpl
+    private val itemRepository: ItemListRepositoryImpl
 ) : MVI.Store<ItemStore.Data, ItemStore.Intent>() {
 
     data class Data(
-        var itemList : List<Item> ? = null,
+        var itemList: List<Item>? = null,
     )
 
-    @ExperimentalCoroutinesApi
-    sealed class Intent : MVI.Store.Intent(){
+    sealed class Intent : MVI.Store.Intent() {
         class LoadAllItems : Intent()
         class InsertNewItem(
-            val description : String,
-            val quantity : Int
+            val description: String,
+            val quantity: Int
         ) : Intent()
     }
 
     override fun initialData(): Data = Data()
 
     override suspend fun resolveIntent(intent: Intent) =
-        when(intent){
+        when (intent) {
             is Intent.LoadAllItems -> reducerLoadCustomerWithDetails(intent)
             is Intent.InsertNewItem -> reducerInsertNewItem(intent)
         }
 
 
-    private suspend fun reducerLoadCustomerWithDetails(intent: Intent.LoadAllItems) = produceReducer { setState ->
+    private suspend fun reducerLoadCustomerWithDetails(intent: Intent.LoadAllItems) =
+        produceReducer { setState ->
             val itemList = itemRepository.getAllItems()
 
             setState(
@@ -44,20 +42,23 @@ class ItemStore(
             )
         }
 
-    private suspend fun reducerInsertNewItem(intent : Intent.InsertNewItem) = produceReducer {setState ->
-        itemRepository.insertNewItem(Item(
-            0,
-            intent.description,
-            intent.quantity
-        ))
-
-        setState(
-            getState().copy(
-                message = Message(MessageType.SUCCESS,null,"Item cadastrado!")
+    private suspend fun reducerInsertNewItem(intent: Intent.InsertNewItem) =
+        produceReducer { setState ->
+            itemRepository.insertNewItem(
+                Item(
+                    0,
+                    intent.description,
+                    intent.quantity
+                )
             )
-        )
 
-    }
+            setState(
+                getState().copy(
+                    message = Message(MessageType.SUCCESS, null, "Item cadastrado!")
+                )
+            )
+
+        }
 
     fun actionLoadItems() = produceAction { dispatch ->
         dispatch(MVI.Store.Intent.LoadingIntent(loading = true))
@@ -65,14 +66,14 @@ class ItemStore(
         dispatch(MVI.Store.Intent.LoadingIntent(loading = false))
     }
 
-    fun actionInsertItem(description : String, quantity : Int) = produceAction { dispatch ->
+    fun actionInsertItem(description: String, quantity: Int) = produceAction { dispatch ->
         dispatch(MVI.Store.Intent.LoadingIntent(loading = true))
-        dispatch(Intent.InsertNewItem(description,quantity))
+        dispatch(Intent.InsertNewItem(description, quantity))
         dispatch(MVI.Store.Intent.LoadingIntent(loading = false))
     }
 
-    fun actionClearMessage() = produceAction {
-
+    fun actionClearMessage() = produceAction { dispatch ->
+        dispatch(MVI.Store.Intent.MessageIntent(null))
     }
 
 
